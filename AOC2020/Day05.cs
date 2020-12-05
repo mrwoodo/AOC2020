49adae315ff60c07d8b9f643024fba2c5cde51e0
Chris Woodworth <chris.woodworth@icloud.com>
2020-12-05 19:50:39 +1100

    Day 05

--- AOC2020/Day05.cs
+++ AOC2020/Day05.cs
@@ -7,10 +7,13 @@ namespace AOC2020
 {
     public class Day05 : DayBase, ITwoPartQuestion
     {
+        public List<string> Lines = new List<string>();
+        public List<int> SeatIDs = new List<int>();
+
         public Day05()
         {
-            var x = (from line in File.ReadAllLines("Input\\Day05.txt")
-                        select int.Parse(line)).ToList();
+            Lines = (from line in File.ReadAllLines("Input\\Day05.txt")
+                     select line).ToList();
 
             Console.WriteLine(Part1());
             Console.WriteLine(Part2());
@@ -18,12 +21,47 @@ namespace AOC2020
 
         public string Part1()
         {
-            return String.Empty;
+            foreach (var ticket in Lines)
+                SeatIDs.Add(GetSeatID(
+                    rowPartition: ticket.Substring(0, 7), 
+                    seatPartition: ticket[7..]));
+
+            return SeatIDs.Max().ToString();
         }
 
         public string Part2()
         {
+            var sorted = SeatIDs.OrderBy(x => x).ToList();
+
+            for (int i = 0; i < sorted.Count; i++)
+                if ((sorted[i] + 1) != sorted[i + 1])
+                    return (sorted[i] + 1).ToString();
+
             return String.Empty;
+        }
+
+        private int GetSeatID(string rowPartition, string seatPartition)
+        {
+            var rowNum = GetNumberFromPartition(rowPartition, 'F');
+            var seatNum = GetNumberFromPartition(seatPartition, 'L');
+
+            return rowNum * 8 + seatNum;
+        }
+
+        private int GetNumberFromPartition(string partition, char low)
+        {
+            int min = 0;
+            int max = Convert.ToInt32(Math.Pow((double)2, (double)partition.Length) - 1);
+
+            foreach (var c in partition)
+            {
+                if (c == low)
+                    max = (max - min) / 2 + min;
+                else
+                    min = (max - min) / 2 + min + 1;
+            }
+
+            return min;
         }
     }
 }

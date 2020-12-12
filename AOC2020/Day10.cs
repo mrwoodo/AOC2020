7f4870fb455a97fc7e91b4b2e1da15016cd4c416
Chris Woodworth <chris.woodworth@icloud.com>
2020-12-12 21:35:43 +1100

    Half of Day 10, Complete Day 11

---
+++ AOC2020/Day10.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC2020
+{
+    public class Day10 : DayBase, ITwoPartQuestion
+    {
+        public List<int> Devices = new List<int>();
+
+        public Day10()
+        {
+            Devices = (from line in File.ReadAllLines("Input\\Day10.txt")
+                       select int.Parse(line)).OrderBy(i => i).ToList();
+
+            Devices.Insert(0, 0);
+            Devices.Add(Devices.Last() + 3);
+
+            Run(() => Part1(), () => Part2());
+        }
+
+        public string Part1()
+        {
+            var Differences = new Dictionary<int, int>
+            {
+                [1] = 0,
+                [2] = 0,
+                [3] = 0
+            };
+
+            for (int i = 1; i < Devices.Count; i++)
+            {
+                var prev = Devices[i - 1];
+                var curr = Devices[i];
+
+                Differences[curr - prev]++;
+            }
+
+            return $"{Differences[1]}x 1 jolt differences, {Differences[3]}x 3 jolt differences = {Differences[1] * Differences[3]}";
+        }
+
+        public string Part2()
+        {
+            return "";
+        }
+    }
+}
+
+/*
+
+0   1   4   5   6   7   10  11  12  15  16  19  22        1   3   1   1   1   3   1   1   3   1   3   3
+0   1   4   5   6   7   10      12  15  16  19  22        1   3   1   1   1   3       2   3   1   3   3
+0   1   4   5       7   10  11  12  15  16  19  22        1   3   1       2   3   1   1   3   1   3   3
+0   1   4   5       7   10      12  15  16  19  22        1   3   1       2   3       2   3   1   3   3
+0   1   4       6   7   10  11  12  15  16  19  22        1   3       2   1   3   1   1   3   1   3   3
+0   1   4       6   7   10      12  15  16  19  22        1   3       2   1   3       2   3   1   3   3
+0   1   4           7   10  11  12  15  16  19  22        1   3           3   3   1   1   3   1   3   3
+0   1   4           7   10      12  15  16  19  22        1   3           3   3       2   3   1   3   3
+
+                                                          x   x    ---------  x    ---    x   x   x   x
+                                                                        4             2
+
+
+4n
+
+111     111
+12      111
+21      111
+3       111
+
+111     12
+12      12
+21      12
+3       12
+
+111     21
+12      21
+21      21
+3       21
+
+111     3
+12      3
+21      3
+3       3
+
+
+
+
+
+
+
+
+
+[0]: 0
+[1]: 1                  1
+[2]: 2                  1
+[3]: 3                  1   3
+[4]: 4                  1   1
+[5]: 7                  3   3
+[6]: 8                  1
+[7]: 9                  1
+[8]: 10                 1   3
+[9]: 11                 1   1
+[10]: 14                3   3
+[11]: 17                3   3
+[12]: 18                1
+[13]: 19                1
+[14]: 20                1   3
+[15]: 23                3   3
+[16]: 24                1
+[17]: 25                1   2
+[18]: 28                3   3
+[19]: 31                3   3
+[20]: 32                1
+[21]: 33                1
+[22]: 34                1   3
+[23]: 35                1   1
+[24]: 38                3   3
+[25]: 39                1   1
+[26]: 42                3   3
+[27]: 45                3   3
+[28]: 46                1
+[29]: 47                1
+[30]: 48                1   3
+[31]: 49                1   1
+[32]: 52                3   3
+
+
+
+[1]: 1                  1
+[2]: 2                  1
+[3]: 3                  1   3
+
+[6]: 8                  1
+[7]: 9                  1
+[8]: 10                 1   3
+
+[12]: 18                1
+[13]: 19                1
+[14]: 20                1   3
+
+[16]: 24                1
+[17]: 25                1   2
+
+[20]: 32                1
+[21]: 33                1
+[22]: 34                1   3
+
+[28]: 46                1
+[29]: 47                1
+[30]: 48                1   3
+
+
+
+
+
+*/
+
---
+++ AOC2020/Day11.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AOC2020
+{
+    public class Day11 : DayBase, ITwoPartQuestion
+    {
+        public List<string> Lines = new List<string>();
+
+        public Day11()
+        {
+            Lines = (from line in File.ReadAllLines("Input\\Day11.txt")
+                     select line).ToList();
+
+            Run(() => Part1(), () => Part2());
+        }
+
+        public string Part1()
+        {
+            return $"Occupied seats : {Run(mode: 1)}";
+        }
+
+        public string Part2()
+        {
+            return $"Occupied seats : {Run(mode: 2)}";
+        }
+
+        private int Run(int mode)
+        {
+            var room = new Room(Lines, mode);
+            var stateChanged = true;
+            var occupiedSeats = 0;
+
+            while (stateChanged)
+                stateChanged = room.Calculate(out occupiedSeats);
+
+            return occupiedSeats;
+        }
+
+        public class Room
+        {
+            public int Mode;
+            public Seat[,] Seats;
+            public int OccupiedToEmptyAmount => (Mode == 1) ? 4 : 5;
+
+            public int OccupiedSeats
+            {
+                get
+                {
+                    var occupied = 0;
+
+                    foreach (var s in this.Seats)
+                        if (s.State == '#')
+                            occupied++;
+
+                    return occupied;
+                }
+            }
+
+            public Room(List<string> input, int mode)
+            {
+                Mode = mode;
+                Seats = new Seat[input[0].Length, input.Count()];
+
+                for (int x = 0; x < input[0].Length; x++)
+                    for (int y = 0; y < input.Count(); y++)
+                        Seats[x, y] = new Seat(this, x, y, Convert.ToChar(input[y].Substring(x, 1)));
+            }
+
+            public List<Seat> GetNeighbours(int X, int Y)
+            {
+                var result = new List<Seat>();
+
+                if (Mode == 1)
+                {
+                    //Checking the 8 adjacents blocks around the given seat
+                    for (int x = X - 1; x <= X + 1; x++)
+                    {
+                        for (int y = Y - 1; y <= Y + 1; y++)
+                        {
+                            if (!((x == X) && (y == Y)))
+                            {
+                                if (InRoom(x, y))
+                                {
+                                    if (Seats[x, y].State != '.')
+                                        result.Add(Seats[x, y]);
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    //Checking for the first occupied/empty seat in 8 lines of sight
+                    result.Add(SearchLineOfSight(X, Y, +1, +0));
+                    result.Add(SearchLineOfSight(X, Y, +1, +1));
+                    result.Add(SearchLineOfSight(X, Y, +0, +1));
+                    result.Add(SearchLineOfSight(X, Y, -1, +1));
+                    result.Add(SearchLineOfSight(X, Y, -1, +0));
+                    result.Add(SearchLineOfSight(X, Y, -1, -1));
+                    result.Add(SearchLineOfSight(X, Y, +0, -1));
+                    result.Add(SearchLineOfSight(X, Y, +1, -1));
+
+                    result.RemoveAll(i => i == null);
+                }
+
+                return result;
+            }
+
+            private Seat SearchLineOfSight(int x, int y, int xStep, int yStep)
+            {
+                bool finished = false;
+                int counter = 1;
+
+                while (!finished)
+                {
+                    int checkX = xStep * counter + x;
+                    int checkY = yStep * counter + y;
+
+                    if (InRoom(checkX, checkY))
+                    {
+                        if (Seats[checkX, checkY].State != '.')
+                            return Seats[checkX, checkY];
+                        else
+                            counter++;
+                    }
+                    else
+                        finished = true;
+                }
+
+                return null; //fell off the map
+            }
+
+            private bool InRoom(int x, int y)
+            {
+                return ((x >= 0) && (y >= 0) && (x < Seats.GetLength(0)) && (y < Seats.GetLength(1)));
+            }
+
+            public bool Calculate(out int occupiedSeats)
+            {
+                occupiedSeats = 0;
+                var stateChanged = false;
+
+                foreach (var s in Seats)
+                    s.PrepareToCalculate();
+
+                foreach (var s in Seats)
+                {
+                    if (s.Calculate())
+                        stateChanged = true;
+
+                    if (s.State == '#')
+                        occupiedSeats++;
+                }
+
+                return stateChanged;
+            }
+        }
+
+        public class Seat
+        {
+            public char State { get; set; }
+            private readonly Room _room;
+            private readonly int X;
+            private readonly int Y;
+            private bool StateChanged { get; set; }
+            private List<Seat> neighbours;
+
+            public Seat(Room room, int x, int y, char state)
+            {
+                _room = room;
+                X = x;
+                Y = y;
+                State = state;
+            }
+
+            public void PrepareToCalculate()
+            {
+                neighbours = new List<Seat>();
+
+                foreach (var n in _room.GetNeighbours(X, Y))
+                    neighbours.Add(new Seat(this._room, n.X, n.Y, n.State));
+            }
+
+            public bool Calculate()
+            {
+                this.StateChanged = false;
+
+                // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+                if ((this.State == 'L') && !neighbours.Any(i => i.State == '#'))
+                {
+                    this.State = '#';
+                    this.StateChanged = true;
+                }
+                //If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+                else if ((this.State == '#') && (neighbours.Count(i => i.State == '#') >= _room.OccupiedToEmptyAmount))
+                {
+                    this.State = 'L';
+                    this.StateChanged = true;
+                }
+
+                return this.StateChanged;
+            }
+        }
+    }
+}

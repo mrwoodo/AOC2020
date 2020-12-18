d2d1fcfa453da10be3c019cac9aca3dd92a8b159
Chris Woodworth <chris.woodworth@icloud.com>
2020-12-18 19:21:08 +1100

    Day 17 - 3D and 4D world now using same class - But solving 4D takes 3 minutes to run

--- AOC2020/Day17.cs
+++ AOC2020/Day17.cs
@@ -19,7 +19,7 @@ namespace AOC2020
 
         public string Part1()
         {
-            var world = new World3D(Lines);
+            var world = new World(Lines, fourD: false);
             world.Play(6);
 
             return $"3D world Active Points: {world.ActivePoints}";
@@ -27,7 +27,7 @@ namespace AOC2020
 
         public string Part2()
         {
-            var world = new World4D(Lines);
+            var world = new World(Lines, fourD: true);
             world.Play(6);
 
             return $"4D world Active Points: {world.ActivePoints}";
@@ -66,129 +66,7 @@ namespace AOC2020
 
         }
 
-        public class World3D
-        {
-            private readonly List<Point> Points = new List<Point>();
-            public int ActivePoints => Points.Count(i => i.Active);
-            private readonly Dictionary<string, int> PointCache = new Dictionary<string, int>();
-            private int Xmin = 0;
-            private int Xmax = 0;
-            private int Ymin = 0;
-            private int Ymax = 0;
-            private int Zmin = 0;
-            private int Zmax = 0;
-
-            public World3D()
-            {
-                Points = new List<Point>();
-            }
-
-            public World3D(List<string> input) : base()
-            {
-                for (int y = 0; y < input.Count; y++)
-                    for (int x = 0; x < input[y].Length; x++)
-                        AddPoint(new Point(x, -y, 0, input[y][x] == '#'), true);
-            }
-
-            public void Play(int turns)
-            {
-                for (var turn = 1; turn <= turns; turn++)
-                {
-                    for (int x = Xmin - 1; x <= Xmax + 1; x++)
-                        for (int y = Ymin - 1; y <= Ymax + 1; y++)
-                            for (int z = Zmin - 1; z <= Zmax + 1; z++)
-                                GetPoint(x, y, z, false);
-
-                    var count = Points.Count;
-
-                    for (int i = 0; i < count; i++)
-                    {
-                        var p = Points[i];
-                        var neighbours = GetNeighbours(p);
-                        var activeNeighbours = neighbours.Count(i => i.Active);
-
-                        //If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active.Otherwise, the cube becomes inactive.
-                        if (p.Active && ((activeNeighbours < 2) || (activeNeighbours > 3)))
-                        {
-                            p.NewActive = false;
-                            p.StateChanged = true;
-                        }
-
-                        //If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active. Otherwise, the cube remains inactive.
-                        if (!p.Active && (activeNeighbours == 3))
-                        {
-                            p.NewActive = true;
-                            p.StateChanged = true;
-                        }
-                    }
-
-                    foreach (var p in Points.Where(i => i.StateChanged))
-                    {
-                        p.Active = p.NewActive;
-                        p.StateChanged = false;
-                    }
-                }
-            }
-
-            private Point[] GetNeighbours(Point p)
-            {
-                var result = new Point[26];
-                var counter = 0;
-
-                for (int x = p.X - 1; x <= p.X + 1; x++)
-                {
-                    for (int y = p.Y - 1; y <= p.Y + 1; y++)
-                    {
-                        for (int z = p.Z - 1; z <= p.Z + 1; z++)
-                        {
-                            if ((x == p.X) && (y == p.Y) && (z == p.Z))
-                            { }
-                            else
-                            {
-                                result[counter] = GetPoint(x, y, z, false);
-                                counter++;
-                            }
-                        }
-                    }
-                }
-
-                return result;
-            }
-
-            private Point GetPoint(int x, int y, int z, bool updateBoundary)
-            {
-                var cacheKey = $"{x},{y},{z}";
-                if (PointCache.ContainsKey(cacheKey))
-                    return Points[PointCache[cacheKey]];
-
-                var search = Points.Where(i => ((i.X == x) && (i.Y == y) && (i.Z == z))).FirstOrDefault();
-                if (search == null)
-                {
-                    search = new Point(x, y, z);
-                    AddPoint(search, updateBoundary);
-                    PointCache[cacheKey] = Points.Count - 1;
-                }
-
-                return search;
-            }
-
-            private void AddPoint(Point p, bool updateBoundary)
-            {
-                Points.Add(p);
-
-                if (updateBoundary)
-                {
-                    if (p.X < Xmin) Xmin = p.X;
-                    if (p.X > Xmax) Xmax = p.X;
-                    if (p.Y < Ymin) Ymin = p.Y;
-                    if (p.Y > Ymax) Ymax = p.Y;
-                    if (p.Z < Zmin) Zmin = p.Z;
-                    if (p.Z > Zmax) Zmax = p.Z;
-                }
-            }
-        }
-
-        public class World4D
+        public class World
         {
             private readonly List<Point> Points = new List<Point>();
             public int ActivePoints => Points.Count(i => i.Active);
@@ -201,14 +79,17 @@ namespace AOC2020
             private int Zmax = 0;
             private int Amin = 0;
             private int Amax = 0;
+            private bool FourD = false;
 
-            public World4D()
+            public World(bool fourD)
             {
+                FourD = fourD;
                 Points = new List<Point>();
             }
 
-            public World4D(List<string> input) : base()
+            public World(List<string> input, bool fourD)
             {
+                FourD = fourD;
                 for (int y = 0; y < input.Count; y++)
                     for (int x = 0; x < input[y].Length; x++)
                         AddPoint(new Point(x, -y, 0, 0, input[y][x] == '#'), true);
@@ -221,7 +102,7 @@ namespace AOC2020
                     for (int x = Xmin - 1; x <= Xmax + 1; x++)
                         for (int y = Ymin - 1; y <= Ymax + 1; y++)
                             for (int z = Zmin - 1; z <= Zmax + 1; z++)
-                                for (int a = Amin - 1; a <= Amax + 1; a++)
+                                for (int a = (FourD ? (Amin - 1) : 0); a <= (FourD ? (Amax + 1) : 0); a++)
                                     GetPoint(x, y, z, a, false);
 
                     var count = Points.Count;
@@ -257,7 +138,7 @@ namespace AOC2020
 
             private Point[] GetNeighbours(Point p)
             {
-                var result = new Point[80];
+                var result = (FourD ? new Point[80] : new Point[26]);
                 var counter = 0;
 
                 for (int x = p.X - 1; x <= p.X + 1; x++)
@@ -266,7 +147,7 @@ namespace AOC2020
                     {
                         for (int z = p.Z - 1; z <= p.Z + 1; z++)
                         {
-                            for (int a = p.A - 1; a <= p.A + 1; a++)
+                            for (int a = (FourD ? (p.A - 1) : 0); a <= (FourD ? (p.A + 1) : 0); a++)
                             {
                                 if ((x == p.X) && (y == p.Y) && (z == p.Z) && (a == p.A))
                                 { }

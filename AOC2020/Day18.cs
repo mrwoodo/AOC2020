9a070c6e63402e596147a2ae411b61372f9d82cf
Chris Woodworth <chris.woodworth@icloud.com>
2020-12-19 20:20:19 +1100

    Day 18 Part 2

--- AOC2020/Day18.cs
+++ AOC2020/Day18.cs
@@ -31,14 +31,26 @@ namespace AOC2020
 
         public string Part2()
         {
-            return "";
+            var c = new Calculator(Advanced: true);
+            long sum = 0;
+
+            foreach (var i in Lines)
+                sum += c.Calculate(i);
+
+            return $"Sum = {sum}";
         }
     }
 
     public class Calculator
     {
-        List<int> Levels;
-        StringBuilder Line;
+        private List<int> Levels;
+        private StringBuilder Line;
+        private bool AdvancedMode;
+
+        public Calculator(bool Advanced = false)
+        {
+            AdvancedMode = Advanced;
+        }
 
         public long Calculate(string l)
         {
@@ -93,13 +105,17 @@ namespace AOC2020
 
             //strip out the bracket and calculate its components
             var sub = Line.ToString().Substring(start, end - start + 1);
-            result = CalculateBracket(sub);
+
+            if (AdvancedMode)
+                result = CalculateBracketAdvanced(sub);
+            else
+                result = CalculateBracket(sub);
 
             //remove the bracket from the original
             Line.Remove(start, end - start + 1);
             Levels.RemoveRange(start, end - start + 1);
 
-            //and substitute with the calc we just performed
+            //and substitute with the result we just worked out
             var replace = result.ToString();
             for (int x = 0; x < replace.Length; x++)
             {
@@ -118,6 +134,34 @@ namespace AOC2020
                 if (split[i] == "+")
                     result += long.Parse(split[i + 1]);
                 else if (split[i] == "*")
+                    result *= long.Parse(split[i + 1]);
+            }
+
+            return result;
+        }
+
+        private long CalculateBracketAdvanced(string s)
+        {
+            var split = s.Replace("(", "").Replace(")", "").Split(" ").ToList();
+
+            //in advanced, we do addition before multiplication
+            while (split.Where(i => i == "+").Count() > 0)
+            {
+                for (int i = 1; i < split.Count; i++)
+                {
+                    if (split[i] == "+")
+                    {
+                        split[i - 1] = (long.Parse(split[i - 1]) + long.Parse(split[i + 1])).ToString();
+                        split.RemoveRange(i, 2);
+                        break;
+                    }
+                }
+            }
+
+            var result = long.Parse(split[0]);
+            for (int i = 1; i < split.Count; i++)
+            {
+                if (split[i] == "*")
                     result *= long.Parse(split[i + 1]);
             }
 

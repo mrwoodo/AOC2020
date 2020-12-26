6a03cbe2f2ac2bdd08495abc0b3342f644009d58
Chris Woodworth <chris.woodworth@icloud.com>
2020-12-27 10:33:57 +1100

    Day 23 Part 1

--- AOC2020/Day23.cs
+++ AOC2020/Day23.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,13 @@ namespace AOC2020
 {
     public class Day23 : DayBase, ITwoPartQuestion
     {
+        private LinkedList<int> Cups = new LinkedList<int>();
+        private LinkedList<int> PickedUp = new LinkedList<int>();
+        private LinkedListNode<int> CurrentCup;
+        private int LowestCupVal = 0;
+        private int HighestCupVal = 0;
+        private const int CUPS_MOVE = 3;
+
         public Day23()
         {
             Run(() => Part1(), () => Part2());
@@ -15,12 +23,92 @@ namespace AOC2020
 
         public string Part1()
         {
-            return "";
+            foreach (var c in (from c in InputFile select int.Parse(c.ToString())))
+                Cups.AddLast(c);
+
+            CurrentCup = Cups.First;
+            LowestCupVal = Cups.Min();
+            HighestCupVal = Cups.Max();
+
+            PlayMove(100);
+
+            var result = "";
+            foreach (var c in Cups)
+                result += c.ToString();
+
+            var start = result.IndexOf("1");
+            return $"Cup Sequence = {result[(start + 1)..]}{result.Substring(0, start)}";
         }
 
         public string Part2()
         {
+            foreach (var c in (from c in InputFile select int.Parse(c.ToString())))
+                Cups.AddLast(c);
+
+            var i = Cups.Max() + 1;
+            for (int l = i; l <= 1000000; l++)
+                Cups.AddLast(l);
+
+            CurrentCup = Cups.First;
+            LowestCupVal = Cups.Min();
+            HighestCupVal = Cups.Max();
+
+            PlayMove(10000000);
+
             return "";
+        }
+
+        private void PlayMove(int moves)
+        {
+            for (int m = 0; m < moves; m++)
+            {
+                for (int i = 0; i < CUPS_MOVE; i++)
+                {
+                    var cup = Clockwise(CurrentCup);
+                    PickedUp.AddLast(cup.Value);
+                    Cups.Remove(cup);
+                }
+
+                var dest = DestinationCup(CurrentCup.Value - 1);
+                for (int i = 0; i < CUPS_MOVE; i++)
+                {
+                    dest = Cups.AddAfter(dest, PickedUp.First());
+                    PickedUp.RemoveFirst();
+                }
+
+                CurrentCup = Clockwise(CurrentCup);
+            }
+        }
+
+        private LinkedListNode<int> Clockwise(LinkedListNode<int> cup)
+        {
+            //Not bothering with circular list
+            if (cup.Next == null)
+                return cup.List.First;
+
+            return cup.Next;
+        }
+
+        private LinkedListNode<int> DestinationCup(int dest)
+        {
+            while (true)
+            {
+                if (PickedUp.Contains(dest))
+                    dest--;
+
+                if (dest < LowestCupVal)
+                    dest = HighestCupVal;
+
+                if (!PickedUp.Contains(dest))
+                {
+                    var search = Cups.Find(dest);
+                    if (search != null)
+
+                        return search;
+
+                    dest--;
+                }
+            }
         }
     }
 }
--- AOC2020/Program.cs
+++ AOC2020/Program.cs
@@ -13,7 +13,7 @@ namespace AOC2020
             //foreach (var day in days)
             //    Activator.CreateInstance(day);
 
-            _ = new Day25();
+            _ = new Day23();
 
             Console.ReadLine();
         }
